44c23eadf1ca0b58b5b57dae05605c7618805327
Helena <helena@klarr.se>
2024-09-17 13:36:54 +0200

    added product controller

--- src/main/java/com/dog/shop/controllers/ProductController.java
+++ src/main/java/com/dog/shop/controllers/ProductController.java
@@ -1,58 +1,92 @@
 package com.dog.shop.controllers;
 
-import com.dog.shop.dto.ProductDTO;
 import com.dog.shop.models.Product;
 import com.dog.shop.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
+    private final ProductService<Product> productService;
+
     @Autowired
-    private ProductService productService;
+    public ProductController(ProductService<Product> productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Product>> getProductById(@PathVariable String id) {
+        Optional<Product> product = productService.getProductById(id);
+        return ResponseEntity.ok(product);
+    }
 
     @PostMapping
-    public Product createProduct(@RequestBody ProductDTO productDTO) {
-        return productService.createProduct(productDTO);  // Vidarebefordra DTO till servicelagret
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product createdProduct = productService.createProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
     }
 
-    @GetMapping("/name/{name}")
-    public List<Product> getProductsByName(@PathVariable String name) {
-        return productService.getProductsByName(name);
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable String id, @RequestBody Product product) {
+        Product updateProduct = productService.updateProduct(id, product);
+        return ResponseEntity.ok(updateProduct);
     }
 
-    @GetMapping("/price")
-    public List<Product> getProductsByPriceRange(@RequestParam double minPrice, @RequestParam double maxPrice) {
-        return productService.getProductsByPriceRange(minPrice, maxPrice);
-    }
-
-    @GetMapping("/color/{color}")
-    public List<Product> getProductsByColor(@PathVariable String color) {
-        return productService.getProductsByColor(color);
-    }
-
-    @GetMapping("/size-material")
-    public List<Product> getProductsBySizeAndMaterial(@RequestParam String size, @RequestParam String material) {
-        return productService.getProductsBySizeAndMaterial(size, material);
-    }
-
-    @GetMapping("/length/{length}")
-    public List<Product> getCollarsByLength(@PathVariable double length) {
-        return productService.getCollarsByLength(length);
-    }
-
-    @GetMapping("/type/{type}")
-    public List<Product> getToysByType(@PathVariable String type) {
-        return productService.getToysByType(type);
-    }
-
-    @GetMapping("/capacity/{capacity}")
-    public List<Product> getBowlsByCapacity(@PathVariable double capacity) {
-        return productService.getBowlsByCapacity(capacity);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable String id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
